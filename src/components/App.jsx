c6ae099cce97a8492d0dfb2ec4cad8804b5a0564
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2024-01-17 21:14:24 +0200

    commit2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import ContactList from '../components/ContactList/ContactList';
+import Form from '../components/Form/Form';
+import Filter from '../components/Filter/Filter';
+
+const App = () => {
+ 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
+
+export default App;
+
+
